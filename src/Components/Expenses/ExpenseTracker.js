e78d914ea2c7f0240a9598b50432e17aedc43a82
bhanu567 <bhanupjha5465@gmail.com>
2024-01-10 10:29:30 +0530

    added a Update expense to 100$ with react and DOM seperately

--- src/Components/Expenses/ExpenseTracker.js
+++ src/Components/Expenses/ExpenseTracker.js
@@ -1,3 +1,4 @@
+import react, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
@@ -5,18 +6,37 @@ import Card from "../UI/Card";
 
 
 const ExpenseTracker = (props) => {
-  const clickHandler = (e) => {
+  const [amount, setAmount] = useState(props.expensesData.amount);
+  const clickHandler2 = (e) => {
+    setAmount("100");
+  }
+  // const clickHandler2 = (e) => {
+  //   console.log(e.target);
+  //   e.target.parentElement.innerHTML = "100";
+  // }
+  const [title, setTitle] = useState(props.expensesData.title);
+  const clickHandler1 = (e) => {
+    setTitle("Updated!");
+  }
+  // const clickHandler1 = (e) => {
+  //   e.target.parentElement.innerHTML = "Updated!";
+  //   console.log(e.target);
+  // }
+
+  const clickHandler3 = (e) => {
     e.target.parentElement.remove();
   }
   return (
     <Card className="expense-item">
       <ExpenseDate Date={props.expensesData.date}></ExpenseDate>
       <ExpenseDetails
-        title={props.expensesData.title}
+        title={title}
         LocationOfExpenditure={props.expensesData.LocationOfExpenditure}
-        amount={props.expensesData.amount}
+        amount={amount}
       ></ExpenseDetails>
-      <button onClick={clickHandler}>Delete Expense</button>
+      <button onClick={clickHandler1}>Edit Expense Details</button>
+      <button onClick={clickHandler2}>Edit Expense Cost</button>
+      <button onClick={clickHandler3}>Delete Expense</button>
     </Card>
   );
 }
